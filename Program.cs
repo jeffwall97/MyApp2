b53d71f628e55f86532f947fa8727deac95f1c86
Jeff Wallsinger <jeffwall97@gmail.com>
2018-10-09 20:47:33 -0400

    commit 4

--- Program.cs
+++ Program.cs
@@ -7,18 +7,8 @@ namespace list_quickstart
     {
         static void Main(string[] args)
         {
-            //WorkingWithStrings();
-            var fibonacciNumbers = new List<int> { 1, 1 };
-
-            while (fibonacciNumbers.Count < 20)
-            {
-                var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
-                var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
-
-                fibonacciNumbers.Add(previous + previous2);
-            }
-            foreach (var item in fibonacciNumbers)
-                Console.WriteLine($"Number = {item}");
+            WorkingWithStrings();
+            PrintFibonacciNumbers();
         }
 
         public static void WorkingWithStrings()
@@ -66,6 +56,21 @@ namespace list_quickstart
             {
                 Console.WriteLine($"Hello {name.ToUpper()}!");
             }
+        }
+        public static void PrintFibonacciNumbers()
+        {
+            var fibonacciNumbers = new List<int> { 1, 1 };
+
+            while (fibonacciNumbers.Count < 20)
+            {
+                var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+                var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+                fibonacciNumbers.Add(previous + previous2);
+            }
+
+            foreach (var item in fibonacciNumbers)
+                Console.WriteLine($"Number = {item}");
         }
     }
 }
